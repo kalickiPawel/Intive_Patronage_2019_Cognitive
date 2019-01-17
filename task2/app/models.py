61827b775341628dacb32adff9d650fa909b20a6
Pawel Kalicki <kalicki.paul@icloud.com>
2019-01-17 01:46:34 +0100

    Created new model Estimate for variables to estimation

--- task2/app/models.py
+++ task2/app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
-class EstimateEarning(models.Model):
+class Earning(models.Model):
     
     salary_brutto = models.FloatField()
     worked_years = models.FloatField()
 
     def __str__(self):
         return f'{self.salary_brutto} {self.worked_years}'
+
+class Estimate(models.Model):
+    pred_worked_years = models.FloatField()
+    pred_salary_brutto = models.FloatField()
+
+    def __str__(self):
+        return f'{self.pred_salary_brutto} {self.pred_worked_years}'
