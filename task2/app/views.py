a13b0e84f17256bc0b1d708bb493f9898a0b9ca6
Pawel Kalicki <kalicki.paul@icloud.com>
2019-01-18 05:27:06 +0100

    Added ChartDataClass - rest framework

--- task2/app/views.py
+++ task2/app/views.py
@@ -1,14 +1,35 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Earning
+from .models import Estimate
 
 # Create your views here.
 def home(request):
     return render(request, 'app/base.html', {})
+
+def charts(request):
+    return render(request, 'app/charts.html', {})
     
 def data_table(request):
-    estimate_earnings = Earning.objects.all()
-    return render(request, 'app/data_table.html', {'estimate': estimate_earnings})
 
-def histograms(request):
-    temp = 0
-    return render(request, 'app/histograms.html', {'variable': temp})
+    estimate = Estimate.objects.all()
+    earnings = Earning.objects.all()
+
+    return render(request, 'app/data_table.html', {'earnings': earnings, 'estimate': estimate})
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"]
+        data = {
+                "labels": labels,
+                "earning": Earning.objects.all(),
+                "estimate": Estimate.objects.all()
+        }
+        return Response(data)
