b07766c081f07678dae2e4da8814b4d935e84f99
Pawel Kalicki <kalicki.paul@icloud.com>
2019-01-18 05:27:39 +0100

    Added url to charts and to rest api

--- task2/app/urls.py
+++ task2/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('data_table', views.data_table, name='data_table'),
-    path('histograms', views.histograms, name='histograms'),
+    path('charts', views.charts, name='charts'),
+    path('api/chart/data', views.ChartData.as_view()),
 ]
